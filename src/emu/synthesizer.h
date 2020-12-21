4793c06e567298803f0d17f9ae0065daf7c01280
Daniel Kroening <daniel.kroening@magd.ox.ac.uk>
2020-12-21 16:52:04 +0000

    new interface

--- src/emu/synthesizer.h
+++ src/emu/synthesizer.h
@@ -2,12 +2,22 @@
 #define EMU_SYNTHESIZER_H
 
 #include <functional>
+
 #include <util/expr.h>
 
 class synthesizert
 {
 public:
-  exprt operator()(const std::function<exprt(exprt)> &model);
+  // set up the problem, typcially a formula with quantifiers
+  virtual void set_problem(const exprt &) = 0;
+
+  // add a constraint to the problem
+  virtual void add_constraint(const exprt &) = 0;
+
+  // build a candidate solution, in the form of a model
+  // that can be queried using model()
+  using resultt = enum { CANDIDATE, NO_SOLUTION };
+  virtual resultt operator()() = 0;
 };
 
 #endif /*EMU_SYNTHESIZER_H*/
--- src/emu/verify.h
+++ src/emu/verify.h
@@ -2,12 +2,19 @@
 #define EMU_VERIFY_H
 
 #include <functional>
+
 #include <util/expr.h>
 
 class verifyt
 {
 public:
-  exprt operator()(const std::function<exprt(exprt)> &model);
+  // set up the problem, typically a formula with quantifiers
+  virtual void set_problem(const exprt &) = 0;
+
+  // Verify a candidate solution, given in the form of a model for
+  // the problem.  Returns 'true' if the model is a solution,
+  // and a constraint on the solution otherwise.
+  virtual exprt operator()(const std::function<exprt(exprt)> &model) = 0;
 };
 
 #endif /*EMU_VERIFY_H*/
