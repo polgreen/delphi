4c2bca78effb294110712bb9cdd050412921a879
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2020-12-29 14:11:18 +0000

    extend problem.h
    also removed the formula, since the assumptions and constraints need to be stored separately, in order to extend them

--- src/emu/problem.h
+++ src/emu/problem.h
@@ -2,19 +2,22 @@
 #define EMU_PROBLEM_H
 
 #include <util/expr.h>
+#include "sygus_parser.h"
 
 class problemt
 {
 public:
-  explicit problemt(exprt __formula) : formula(std::move(__formula))
-  {
-  }
+  // explicit problemt(exprt __formula) : formula(std::move(__formula))
+  // {
+  // }
 
-  // This is expected not to change
-  const exprt formula;
+  // // This is expected not to change
+  // const exprt formula;
 
+  using oracle_constraint_gent = sygus_parsert::oracle_constraint_gent;
   // We only ever add assumptions and constraints, we never remove them.
   std::vector<exprt> assumptions, constraints;
+  std::vector<oracle_constraint_gent> oracle_assumption_gens, oracle_constraint_gens;
 };
 
 #endif /*EMU_PROBLEM_H*/
