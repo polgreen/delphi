4420a7734d8124cc8801b9564ad415acf3770250
polgreen <epolgreen@gmail.com>
2021-01-25 11:16:02 +0000

    update max-pos

--- examples/second_order/max-pos.cpp
+++ examples/second_order/max-pos.cpp
@@ -4,10 +4,6 @@
 #include <sstream>
 #include <random>
 
-bool randomBool() {
-   return rand() > (RAND_MAX / 2);
-}
-
 int max(int x, int y)
 {
 	if(x>y)
@@ -28,23 +24,14 @@ int main(int argc, const char *argv[])
 		return 1;
 	}
 	srand (time(NULL));
-	int x,y;
-
-	if(randomBool)
-	{
-		x = rand()%50+100;
-		y = 
-
-	}
 	int x = rand();
 	int y = rand();
 
-	std::cout<<x <<" "<< y <<" "<< max(x,y)<<std::endl;
 
-	// std::cout<<"(model \n" 
-	// << "(define-fun x () Int "<< x << ")\n"
-	// << "(define-fun y () Int "<< y << ")\n"
-	// << "(define-fun z () Int "<< max(x,y)<< "))\n"; 
+	std::cout<<"(model \n" 
+	<< "(define-fun x () Int "<< x << ")\n"
+	<< "(define-fun y () Int "<< y << ")\n"
+	<< "(define-fun z () Int "<< max(x,y)<< "))\n"; 
 
 	return 0;
 
