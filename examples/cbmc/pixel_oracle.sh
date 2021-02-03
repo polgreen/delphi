f99dc8eb6c7e09d8bfd4d5550cda57eeabf1665b
Elizabeth Polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2021-02-03 18:06:36 +0000

    inverting the cat now works!

--- examples/cbmc/pixel_oracle.sh
+++ examples/cbmc/pixel_oracle.sh
@@ -8,31 +8,48 @@ cat << EOM >> pixel_oracle.c
 
 unsigned char *stbi_load(char const *filename, int *x, int *y, int *channels_in_file, int desired_channels);
 int printf(const char * restrict, ...) __attribute__((__format__ (__printf__, 1, 2)));
+void *malloc(unsigned long);
+
+unsigned char target(unsigned char pixel)
+{
+  return ~ pixel; // invert
+}
 
 int main()
 {
   int x, y, n;
   // get grayscale
-  unsigned char *data = stbi_load("foto.jpg", &x, &y, &n, 1);
-  if(data == 0)
+  const unsigned char *data_src = stbi_load("foto.jpg", &x, &y, &n, 1);
+  if(data_src == 0)
     return 1;
 
   // number of pixels
   const int pixels = x * y;
 
-  // transform using given function
-  for(int index = 0; index < pixels; index++)
-    data[index] = tweak(data[index]);
+  // make two copies of the image
+  unsigned char *data_target = malloc(pixels);
+  unsigned char *data_tweak = malloc(pixels);
 
-  // compute the average
-  unsigned long long sum = 0;
+  // transform using reference function
   for(int index = 0; index < pixels; index++)
-    sum += data[index];
- 
-  unsigned char average = sum/pixels;
- 
-  // output average in SMT-LIB format
-  printf("(_ bv%d 8)\n", average);
+    data_target[index] = target(data_src[index]);
+
+  // transform using synthesized function
+  for(int index = 0; index < pixels; index++)
+    data_tweak[index] = tweak(data_src[index]);
+
+  // compare with target
+  for(int index = 0; index < pixels; index++)
+  {
+    if(data_target[index] != data_tweak[index])
+    {
+      // they differ; report the expected mapping
+      printf("false (_ bv%d 8) (_ bv%d 8)\n", data_src[index], data_target[index]);
+    }
+  }
+
+  // they match
+  printf("true (_ bv%d 8) (_ bv%d 8)\n", 0, 0);
 }
 EOM
 
