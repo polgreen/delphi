775ba6d426f8bd96ea1ba4938d5aa41e0a12c8c0
Elizabeth Polgreen <epolgreen@gmail.com>
2021-04-07 14:16:00 +0100

    add second oracle solver based on negating property

--- src/emu/verification/oracle_solver2.cpp
+++ src/emu/verification/oracle_solver2.cpp
@@ -12,36 +12,18 @@
 #include <sstream>
 #include <iostream>
 
-oracle_solvert::oracle_solvert(
+negation_oracle_solvert::negation_oracle_solvert(
   decision_proceduret &__negation_sub_solver,
     decision_proceduret &__sub_solver,
   message_handlert &__message_handler) :
-  negation_sub_solver(__negation_sub_solver),
-  sub_solver(__sub_solver),
-  log(__message_handler)
+  oracle_solvert(__sub_solver, __message_handler),
+    negation_sub_solver(__negation_sub_solver)
 {
 }
 
-exprt oracle_solvert::get_oracle_value(const function_application_exprt &oracle_app, const std::vector<exprt> &inputs)
-{
-  for(const auto &app : applications)
-  {
-    if(to_function_application_expr(app.first).function() == oracle_app.function())
-    {
-      auto history = oracle_call_history.find(app.second.binary_name);
-      INVARIANT(history != oracle_call_history.end(), "No history for oracle");
-      auto result = history->second.find(inputs);
-      if(result==history->second.end())
-      {
-        return call_oracle(app.second, inputs);        
-      }
-      return result->second;
-    }
-  }
-  return nil_exprt();
-}
 
-void oracle_solvert::set_to(const exprt &expr, bool value)
+
+void negation_oracle_solvert::set_to(const exprt &expr, bool value)
 {
   PRECONDITION(oracle_fun_map != nullptr);
 
@@ -79,121 +61,9 @@ void oracle_solvert::set_to(const exprt &expr, bool value)
   negation_sub_solver.set_to(expr, !value);
 }
 
-exprt oracle_solvert::handle(const exprt &expr)
-{
-  if(expr.is_constant())
-    return expr;
-  else
-  {
-    symbol_exprt symbol_expr("H"+std::to_string(handle_counter++), expr.type());
-    auto equality = equal_exprt(symbol_expr, expr);
-    set_to_true(equality);
-    return std::move(symbol_expr);
-  }
-}
 
-exprt oracle_solvert::get(const exprt &expr) const
-{
-  return sub_solver.get(expr);
-}
 
-void oracle_solvert::print_assignment(std::ostream &out) const
-{
-  sub_solver.print_assignment(out);
-}
-
-std::string oracle_solvert::decision_procedure_text() const
-{
-  return "oracles + " + sub_solver.decision_procedure_text();
-}
-
-oracle_solvert::check_resultt oracle_solvert::check_oracles()
-{
-  oracle_solvert::check_resultt result = CONSISTENT;
-
-  for(const auto &application : applications)
-  {
-    switch(check_oracle(application.first, application.second))
-    {
-    case INCONSISTENT:
-      result = INCONSISTENT;
-      break;
-
-    case CONSISTENT:
-      break;
-
-    case ERROR:
-      return ERROR; // abort
-    }
-  }
-
-  return result;
-}
-
-exprt oracle_solvert::make_oracle_call(const std::string &binary_name, const std::vector<std::string> &argv)
-{
-  log.status() << "Running oracle (verification) ";
-  for (auto &arg : argv)
-    log.status() << ' ' << arg;
-  log.status() << messaget::eom;
-
-  // run the oracle binary
-  std::ostringstream stdout_stream;
-
-  auto run_result = run(
-      binary_name,
-      argv,
-      "",
-      stdout_stream,
-      "");
-
-  if (run_result != 0 && run_result !=10)
-  {
-    log.status() << "oracle " << binary_name << " has failed" << messaget::eom;
-    assert(0);
-    return nil_exprt();
-  }
-  // we assume that the oracle returns the result in SMT-LIB format
-  std::istringstream oracle_response_istream(stdout_stream.str());
-  return oracle_response_parser(oracle_response_istream);
-}
-
-exprt oracle_solvert::call_oracle(
-    const applicationt &application, const std::vector<exprt> &inputs)
-{
-  if(cache && oracle_call_history.find(application.binary_name) == oracle_call_history.end())
-  {
-    oracle_call_history[application.binary_name] = oracle_historyt();
-  }
-
-  exprt response;
-
-  if (oracle_call_history[application.binary_name].find(inputs) == oracle_call_history[application.binary_name].end() || !cache)
-  {
-    std::vector<std::string> argv;
-    argv.push_back(application.binary_name);
-
-    for (const auto &input : inputs)
-    {
-      std::ostringstream stream;
-      stream << format(input);
-      argv.push_back(stream.str());
-    }
-
-    response = make_oracle_call(application.binary_name, argv);
-    log.status() << "oracle response " << expr2sygus(response) << messaget::eom;
-    if (cache)
-      oracle_call_history[application.binary_name][inputs] = response;
-  }
-  else
-  {
-    response = oracle_call_history[application.binary_name][inputs];
-  }
-
-  return response;
-}
-
-oracle_solvert::check_resultt oracle_solvert::check_oracle(
+negation_oracle_solvert::check_resultt negation_oracle_solvert::check_oracle(
   const function_application_exprt &application_expr,
   const applicationt &application)
 {
@@ -214,7 +84,7 @@ oracle_solvert::check_resultt oracle_solvert::check_oracle(
 
 
   // check whether the result is consistent with the model
-  if(response == get(application.handle))
+  if(response == negation_sub_solver.get(application.handle))
   {
     log.debug() << "Response matches " << expr2sygus(get(application.handle))<<messaget::eom;
     return CONSISTENT; // done, SAT
@@ -249,7 +119,7 @@ oracle_solvert::check_resultt oracle_solvert::check_oracle(
 
 
 
-decision_proceduret::resultt oracle_solvert::dec_solve()
+decision_proceduret::resultt negation_oracle_solvert::dec_solve()
 {
   PRECONDITION(oracle_fun_map != nullptr);
 
@@ -261,9 +131,10 @@ decision_proceduret::resultt oracle_solvert::dec_solve()
     {
       case resultt::D_UNSATISFIABLE:
         return resultt::D_UNSATISFIABLE;
-      case resultt:D_ERROR:
-        return resultt:D_ERROR;
+      case resultt::D_ERROR:
+        return resultt::D_ERROR;
       case resultt::D_SATISFIABLE:
+        log.debug()<<"Found satisfing counterexample \n";
       // currently do nothing, but consider calling oracles here
     }
 
